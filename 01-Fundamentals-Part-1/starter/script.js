8b29005131809cc2df4d2ad978c44d51611917b3
Jakub Raczek <jakub.raczek@accenture.com>
2025-02-12 15:47:31 +0100

    datatypes

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,28 @@
+let age = 24;
+let firstName = "Ja";
+let fulage = true;
+let childerl;
+firstName = 2;
+// coment
+
+/*
+dsdsd
+*/
+
+console.log(typeof firstName);
+// console.log(firstName + 2);
+
+const ageKuba = 2304 - 1978;
+console.log(ageKuba);
+const ageSarah = 2037 - 2020;
+
+console.log(ageKuba, ageSarah);
+
+const firtsName = "Jakub";
+const lasName = "Kuba";
+console.log(firstName + " " + lasName);
+
+let x = 5 + 5;
+x += 10;
+console.log(x);
+console.log(ageKuba > ageSarah);
